bb3635fc4adef22aec5b144377ca1ebd05fc7acd
oddlyspaced <oddlyspaced1@gmail.com>
2025-08-02 07:43:45 +0530

    refactor: rename files

--- src/hooks/useInventory.ts
+++ src/hooks/useInventory.ts
@@ -1,7 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 import { DUMMY_PRODUCT_DETAILS } from '../data/dummyProductDetailsResponse';
 import { DUMMY_INVENTORY_RESPONSE } from '../data/dummyInventoryResponse';
-import { processInventory } from '../utils/bookingInventoryProcessor';
+import { processInventory } from '../utils/inventoryUtils';
 
 export const useInventory = () => {
 	const { data, isFetching, isLoading } = useQuery({
--- src/utils/inventoryUtils.ts
+++ src/utils/inventoryUtils.ts
@@ -1,56 +1,66 @@
 import dayjs from 'dayjs';
-import { IInventory } from '../types/types';
-import { IPersonPrice, IPriceProfile } from '../types/inventoryTypes';
+import dayjsDuration from 'dayjs/plugin/duration';
+import dayjsUtc from 'dayjs/plugin/utc';
+import { IProductDetails, ITour, IVariant } from '../types/productDetailTypes';
+import {
+	IAvailability,
+	IInventoryResponse,
+	IPriceProfile,
+} from '../types/inventoryTypes';
 
-export type TInventoryVendorIdPriceMap = {
-	[vendorId: number]: number;
+dayjs.extend(dayjsDuration);
+dayjs.extend(dayjsUtc);
+
+/**
+ * Tour Group -> Variant -> Tour -> Date -> Time Slot
+ */
+
+export interface IProcessedTourGroupAvailability {
+	variantAvailabilityMap: TVariantAvailabilityMap;
+	tourIdsInfoMap: TTourInfoMap;
+	variantInfoMap: TVariantInfoMap;
+}
+
+export type TVariantInfoMap = { [variantId: number]: IVariant };
+export type TVariantAvailabilityMap = {
+	[variantId: string]: IProcessedVariantAvailability;
+};
+export interface IProcessedVariantAvailability {
+	variantId?: number;
+	tourAvailabilityMap?: { [tourId: number]: IProcessedTourAvailability };
+}
+
+// TOUR LEVEL DEFINITIONS
+export type TTourInfoMap = {
+	[tourId: number]: TTourDetails;
 };
 
-export type TInventoryTourIdVendorIdMap = {
-	[tourId: number]: TInventoryVendorIdPriceMap;
+export type TTourDetails = {
+	tour: ITour;
+	variantIds: number[];
 };
 
-export type TInventoryTimeTourIdMap = {
-	[time: string]: TInventoryTourIdVendorIdMap;
+export interface IProcessedTourAvailability {
+	tourId: number;
+	firstAvailableDate: dayjs.Dayjs;
+	lastAvailableDate: dayjs.Dayjs;
+	dates: TAvailabilityDateMap;
+}
+
+// DATE LEVEL DEFINITIONS
+export type TAvailabilityDateMap = { [date: string]: IAvailabilityForDate };
+interface IAvailabilityForDate {
+	minPricedAvailability: IAvailability;
+	timeSlotsMap: TAvailabilityTimeMap;
+}
+
+// TIME LEVEL DEFINITIONS
+export type TAvailabilityTimeMap = {
+	[time: string]: IAvailability[];
 };
 
-export type TInventoryDateTimeMap = {
-	[date: string]: TInventoryTimeTourIdMap;
-};
-
-// DATE -> TIME -> TOUR ID -> VENDOR ID -> PRICE
-export const parseInventory = (data: IInventory[]) => {
-	const timeStart = dayjs();
-	const sourceMap: TInventoryDateTimeMap = {};
-	for (let item of data) {
-		if (!sourceMap[item?.date]) {
-			sourceMap[item?.date] = {};
-		}
-		if (!sourceMap[item?.date]?.[item?.time]) {
-			sourceMap[item?.date][item?.time] = {};
-		}
-		if (!sourceMap[item?.date]?.[item?.time]?.[item?.tourId]) {
-			sourceMap[item?.date][item?.time][item?.tourId] = {};
-		}
-		if (
-			!sourceMap[item?.date]?.[item?.time]?.[item?.tourId]?.[
-				item?.vendorId
-			]
-		) {
-			sourceMap[item?.date][item?.time][item?.tourId][item?.vendorId] =
-				item?.price;
-		}
-	}
-
-	const timeEnd = dayjs();
-
-	const timeTaken = timeEnd.diff(timeStart, 'milliseconds');
-	console.log('TEST: ' + timeTaken + ' // ' + data?.length);
-};
-
-export const getDisplayablePriceForPerPersonPricing = (
-	perPersonPaxDetails: IPersonPrice[],
-) => {
+export const getPriceFromPriceProfile = (priceProfile: IPriceProfile) => {
+	const perPersonPaxDetails = priceProfile?.persons;
 	const adultDetails = perPersonPaxDetails.find((paxDetails) => {
 		return paxDetails.type == 'ADULT';
 	});
@@ -61,6 +71,173 @@ export const getDisplayablePriceForPerPersonPricing = (
 	}
 };
 
-export const getPriceFromPriceProfile = (priceProfile: IPriceProfile) => {
-	return getDisplayablePriceForPerPersonPricing(priceProfile?.persons);
+export const processInventory = (
+	productDetails: IProductDetails,
+	inventoryResponse: IInventoryResponse,
+): IProcessedTourGroupAvailability => {
+	const processingStartTime = dayjs();
+	let variantAvailabilityMap: TVariantAvailabilityMap = {};
+	let variantIdToVariantMap: TVariantInfoMap = {};
+	let tourIdsInfoMap: TTourInfoMap = {}; //value will be string of variant IDs
+
+	productDetails.variants.forEach((variant) => {
+		variantIdToVariantMap[variant.id] = variant;
+		variant.tours.forEach((tour) => {
+			if (tourIdsInfoMap[tour.id]) {
+				tourIdsInfoMap[tour.id].variantIds.push(variant.id);
+			} else {
+				tourIdsInfoMap[tour.id] = {
+					tour,
+					variantIds: [variant.id],
+				};
+			}
+		});
+	});
+
+	inventoryResponse.availabilities.forEach((availability) => {
+		const tourDetails = tourIdsInfoMap[availability.tourId];
+		if (tourDetails.variantIds && tourDetails.variantIds.length > 0) {
+			tourDetails.variantIds.forEach((variantId) => {
+				if (variantAvailabilityMap?.[variantId]) {
+					if (
+						variantAvailabilityMap?.[variantId]
+							?.tourAvailabilityMap?.[availability.tourId]
+					) {
+						if (
+							variantAvailabilityMap?.[variantId]
+								.tourAvailabilityMap[availability.tourId].dates[
+								availability.startDate
+							]
+						) {
+							const { minPricedAvailability, timeSlotsMap } =
+								variantAvailabilityMap?.[variantId]
+									.tourAvailabilityMap[availability.tourId]
+									.dates[availability.startDate];
+
+							const currentMinPriceProfile =
+								getPriceFromPriceProfile(
+									minPricedAvailability.priceProfile,
+								);
+
+							const currentAvPriceProfile =
+								getPriceFromPriceProfile(
+									availability.priceProfile,
+								);
+
+							const newMinPriceAvailability =
+								currentMinPriceProfile < currentAvPriceProfile
+									? minPricedAvailability
+									: availability;
+
+							const newTimeSlotsMap = {
+								...timeSlotsMap,
+								[availability.startTime]: [availability],
+							};
+							variantAvailabilityMap[
+								variantId
+							].tourAvailabilityMap[availability.tourId].dates[
+								availability.startDate
+							] = {
+								minPricedAvailability: newMinPriceAvailability,
+								timeSlotsMap: newTimeSlotsMap,
+							};
+						} else {
+							// if variantID + tourId + date does not exist
+							variantAvailabilityMap[
+								variantId
+							].tourAvailabilityMap[availability.tourId].dates[
+								availability.startDate
+							] = {
+								minPricedAvailability: availability,
+								timeSlotsMap: {
+									[availability.startTime]: [availability],
+								},
+							};
+						}
+
+						const currentFirstAvailableDate =
+							variantAvailabilityMap?.[variantId]
+								.tourAvailabilityMap[availability.tourId]
+								.firstAvailableDate;
+						const currentLastAvailableDate =
+							variantAvailabilityMap?.[variantId]
+								.tourAvailabilityMap[availability.tourId]
+								.lastAvailableDate;
+						const availabilityDate = dayjs(availability.startDate);
+						const newFirstAvailableDate =
+							currentFirstAvailableDate.isBefore(availabilityDate)
+								? currentFirstAvailableDate
+								: availabilityDate;
+						const newLastAvailableDate =
+							currentLastAvailableDate.isAfter(availabilityDate)
+								? currentLastAvailableDate
+								: availabilityDate;
+						variantAvailabilityMap[variantId].tourAvailabilityMap[
+							availability.tourId
+						].firstAvailableDate = newFirstAvailableDate;
+						variantAvailabilityMap[variantId].tourAvailabilityMap[
+							availability.tourId
+						].lastAvailableDate = newLastAvailableDate;
+					} else if (
+						variantAvailabilityMap?.[variantId]?.tourAvailabilityMap
+					) {
+						variantAvailabilityMap[variantId].tourAvailabilityMap[
+							availability.tourId
+						] = {
+							firstAvailableDate: dayjs(availability.startDate),
+							lastAvailableDate: dayjs(availability.startDate),
+							tourId: availability.tourId,
+							dates: {
+								[availability.startDate]: {
+									minPricedAvailability: availability,
+									timeSlotsMap: {
+										[availability.startTime]: [
+											availability,
+										],
+									},
+								},
+							},
+						};
+					}
+				} else {
+					// if entry for variant ID itself doesn't exist
+					variantAvailabilityMap[variantId] = {
+						variantId,
+						tourAvailabilityMap: {
+							[availability.tourId]: {
+								firstAvailableDate: dayjs(
+									availability.startDate,
+								),
+								lastAvailableDate: dayjs(
+									availability.startDate,
+								),
+								tourId: availability.tourId,
+								dates: {
+									[availability.startDate]: {
+										minPricedAvailability: availability,
+										timeSlotsMap: {
+											[availability.startTime]: [
+												availability,
+											],
+										},
+									},
+								},
+							},
+						},
+					};
+				}
+			});
+		}
+	});
+
+	const processingEndTime = dayjs();
+	console.log(
+		'PROCESSING TIME: ' +
+			processingEndTime.diff(processingStartTime, 'milliseconds'),
+	);
+	return {
+		tourIdsInfoMap,
+		variantInfoMap: variantIdToVariantMap,
+		variantAvailabilityMap,
+	};
 };
